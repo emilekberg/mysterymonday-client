3501e03cb7becae7f13c6a04cfa0d3a7ebd3e8be
Emil Ekberg <ekberg.emil@gmail.com>
2017-11-15 21:33:21 +0100

    Fixed login system, can now go to any component

---
+++ src/components/authenticated-route.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { Redirect, Route, RouteProps } from "react-router-dom";
+import Network from "../network";
+import Login from "../views/login";
+import { RouterProps } from "react-router";
+
+export const AuthenticatedRoute = (a: any) => {
+	const {component, ...rest} = a;
+	const Component = component;
+	return <Route {...rest} render={ props => {
+		if(Network.isLoggedIn) {
+			return <Component {...props} />;
+		}
+		return <Login {...props} />;
+	}} />;
+};
+export default AuthenticatedRoute;
