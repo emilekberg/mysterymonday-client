ce8d9441b53aa3b7b649ad3e17d84df898da82a4
Emil Ekberg <ekberg.emil@gmail.com>
2017-11-19 21:09:21 +0100

    reoder files to make more sense

--- src/app.tsx
+++ src/app.tsx
@@ -4,20 +4,22 @@ import {BrowserRouter, Route} from "react-router-dom";
 import Login from "./views/login";
 import Signup from "./views/signup";
 import { AuthenticatedRoute } from "./components/authenticated-route";
-import Home from "./views/authenticated/home";
-import Restaurant from "./views/authenticated/restaurant";
+import Home from "./views/auth/home";
+import Restaurant from "./views/auth/restaurant";
 import Header from "./components/header";
 
 class App extends React.Component {
 	public render() {
 		return <BrowserRouter>
 			<div>
+				<Route path="/" component={Header} />
+
 				<Route exact path="/" component={Login} />
 				<Route exact path="/login" component={Login} />
 				<Route exact path="/signup" component={Signup} />
-				<Route path="/" component={Header} />
-				<AuthenticatedRoute exact path="/home" component={Home} />
-				<AuthenticatedRoute exact path="/restaurant" component={Restaurant} />
+
+				<AuthenticatedRoute exact path="/auth/home" component={Home} />
+				<AuthenticatedRoute exact path="/auth/restaurant" component={Restaurant} />
 			</div>
 		</BrowserRouter>;
 	}
