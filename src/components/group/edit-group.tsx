94e891f886ebb35fb7b9393e8a12d60504485b4b
Emil Ekberg <ekberg.emil@gmail.com>
2018-04-02 18:39:23 +0200

    made components stateless for faster rendering

--- src/components/group/edit-group.tsx
+++ src/components/group/edit-group.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import Network from "../../network";
 
-export default class EditGroup extends React.Component<{}, {}>{
-	render() {
-		return <div>
-			<h5>Edit Group</h5>
-		</div>
-	}
+
+export const EditGroup: React.StatelessComponent = () => {
+	return <div>
+		<h5>Edit Group</h5>
+	</div>
 }
+export default EditGroup;
--- src/components/group/groups.tsx
+++ src/components/group/groups.tsx
@@ -3,20 +3,15 @@ import Network from "../../network";
 interface GroupSelectorProps {
 	groups: Array<{name: string}>;
 }
-export default class Groups extends React.Component<GroupSelectorProps, {}>{
-	constructor(props: GroupSelectorProps) {
-		super(props);
-	}
-
-	render() {
-		const groups = this.props.groups.map((group, i) => {
-			return <p key={i}>{group.name}</p>
-		});
-		return <div>
-			<h5>Your Groups</h5>
-			{
-				groups
-			}
-		</div>
-	}
-}
+export const Groups: React.StatelessComponent<GroupSelectorProps> = (props) => {
+	const groups = props.groups.map((group, i) => {
+		return <p key={i}>{group.name}<button>Edit</button></p>
+	});
+	return <div>
+		<h5>Your Groups</h5>
+		{
+			groups
+		}
+	</div>
+};
+export default Groups;
